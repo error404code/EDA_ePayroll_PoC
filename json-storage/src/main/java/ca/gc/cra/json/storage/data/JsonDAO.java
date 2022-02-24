b4fd6096b15b6ac114477a8c8d813912805848f8
error404code <36123327+error404code@users.noreply.github.com>
2022-02-24 15:32:58 +0000

    Update JsonDAO.java

--- json-storage/src/main/java/ca/gc/cra/json/storage/data/JsonDAO.java
+++ json-storage/src/main/java/ca/gc/cra/json/storage/data/JsonDAO.java
@@ -17,10 +17,10 @@ public class JsonDAO {
 	private static final String SQL_INSERT_DATA_END = "\')";
 	
 	private static final String DB_TYPE = "jdbc:postgresql";
-	private static final String DB_HOST = "errorpostgresql"; //"errorpostgresql-eda-epayroll-poc.apps.ocp4.omega.dce-eir.net"
+	private static final String DB_HOST = "epayrollerrordb"; //"errorpostgresql-eda-epayroll-poc.apps.ocp4.omega.dce-eir.net"
 	private static final String DB_PORT = "5432";
 	
-	private static final String BACKUP_DB_NAME = "jsondb";
+	private static final String BACKUP_DB_NAME = "epayrollerrordb";
 	private static final String BACKUP_USER_NAME = "yuriy";
 	private static final String BACKUP_PASSWORD = "1234";
 	
